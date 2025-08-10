4f571a3b7ea116d981455798f2c1296e99285aa9
umer <umeraftab763@gmail.com>
2025-08-10 15:57:47 +0500

    400 issue

--- src/Home.js
+++ src/Home.js
@@ -104,7 +104,7 @@ function Home()
     {
         if(!username)
             return
-        const new_user= (io('/',{auth:{username}})); 
+        const new_user= (io('/',{auth:{username},transports: ['websocket']})); 
         set_socket(new_user)
         new_user.on('connect',()=>
         {
