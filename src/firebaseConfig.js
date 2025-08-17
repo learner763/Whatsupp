c4a5970374ce7e2394fcc11c7ecdb7715910862e
Umer Aftab <167747949+learner763@users.noreply.github.com>
2025-08-17 22:36:17 +0500

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,7 +5,8 @@ const firebaseConfig = {
     storageBucket: "aaaa-90493.firebasestorage.app",
     messagingSenderId: "845602969471",
     appId: "1:845602969471:web:f6fa7e87657a68ff336c4a",
-    measurementId: "G-FFDYCCC490"
+    measurementId: "G-FFDYCCC490",
+    databaseURL: "https://aaaa-90493-default-rtdb.firebaseio.com/"
   };
 
 export default firebaseConfig;
