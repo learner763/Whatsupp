167b187d4ed291b514fa7137818212d3bae8b2f9
Umer Aftab <167747949+learner763@users.noreply.github.com>
2025-08-17 22:35:52 +0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,10 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getDatabase } from "firebase/database";
 import firebaseConfig from "./firebaseConfig";
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+const real_time_db= getDatabase(app)
 
-export { db };
+export { db,real_time_db };
