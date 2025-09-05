e93845a463084217760ef6784fdd5ce003ee01ca
umer <umeraftab763@gmail.com>
2025-09-05 23:28:25 +0500

    added superscript for unread msgs

--- src/Home.js
+++ src/Home.js
@@ -253,7 +253,7 @@ function Home()
                     let to=msgs[msgs.length-1].to;
                     let from=msgs[msgs.length-1].from;
                     let message_text=msgs[msgs.length-1].text;
-                    let time=new Date().toISOString();                    
+                    let time=msgs[msgs.length-1].createdAt===null?new Date().toISOString():msgs[msgs.length-1].createdAt.toDate().toISOString();                    
                     setmessages(prev=>
                     {
                         let previous=[...prev]
