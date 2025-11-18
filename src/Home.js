c7f0904ff79296beff700191641451ab5ac9c30c
umer <umeraftab763@gmail.com>
2025-11-18 18:46:12 +0500

    scroll on msg

--- src/Home.js
+++ src/Home.js
@@ -887,9 +887,7 @@ function Home()
                 if(msg_transfer>1)
                 {
                     if(receiver_again.current===previous[0][0])
-                    {document.getElementsByClassName('chat_detail')[0].children[document.getElementsByClassName('chat_detail')[0].children.length-1].scrollIntoView
-                        ({behavior:'smooth'})
-                    }
+                    {document.getElementsByClassName('chat_detail')[0].children[document.getElementsByClassName('chat_detail')[0].children.length-1].scrollIntoView({behavior:'smooth'})}
                     break
                 }
             }
