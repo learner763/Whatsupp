f1d6b014159732ee3fc569b03f3b67389b65f9fd
umer <umeraftab763@gmail.com>
2025-12-06 17:33:03 +0500

    double tick appears after msg is saved in neondb

--- src/Home.js
+++ src/Home.js
@@ -617,6 +617,8 @@ function Home()
                         {
                             for(let k=0;k<previous[j][1].length;k++)
                             {
+                                console.log(read_messages[i])
+                                console.log(previous[j][1][k])
                                 if(read_messages[i].createdAt.toDate().toISOString()===previous[j][1][k].slice(previous[j][1][k].lastIndexOf(" ")+1,previous[j][1][k].length))
                                 {
                                     previous[j][1][k]=`✔✔${previous[j][1][k]}`
