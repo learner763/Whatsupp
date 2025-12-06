9101df45de3cfae86f3f89fc6c200b681907134d
umer <umeraftab763@gmail.com>
2025-12-06 14:31:35 +0500

    double tick appears after msg is saved in neondb

--- src/Home.js
+++ src/Home.js
@@ -1080,7 +1080,7 @@ function Home()
                                     setmessages(prev=>
                                     {
                                         let previous=[...prev]
-                                        console.log(previous[previous.findIndex(x=>x[0]===receiver)][1][previous[previous.findIndex(x=>x[0]===receiver)][1].findIndex(x=>x===`✔ ${message}     ${set_time_stamp.current}` )]=')
+                                        console.log(previous[previous.findIndex(x=>x[0]===receiver)][1][previous[previous.findIndex(x=>x[0]===receiver)][1].findIndex(x=>x===`✔ ${message}     ${set_time_stamp.current}` )])
                                         previous[previous.findIndex(x=>x[0]===receiver)][1][previous[previous.findIndex(x=>x[0]===receiver)][1].findIndex(x=>x===`✔ ${message}     ${set_time_stamp.current}` )]='✔'+previous[previous.findIndex(x=>x[0]===receiver)][1][previous[previous.findIndex(x=>x[0]===receiver)][1].findIndex(x=>x===`✔ ${message}     ${data.createdAt.toDate().toISOString()}` )]
                                         return previous
                                     })
