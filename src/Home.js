23b2350e94fca775310049040a37d66d202a3854
umer <umeraftab763@gmail.com>
2025-12-10 16:30:41 +0500

    fixing reply  msg issues

--- src/Home.js
+++ src/Home.js
@@ -811,8 +811,9 @@ function Home()
 
     useEffect(()=>
     {
-        if(innerwidth>500){document.getElementById('select_bg').style.width='314px'}
-        else{document.getElementById('select_bg').style.width='243px'}
+        console.log()
+        if(innerwidth>500){document.getElementById('select_bg').style.width=314+'px'}
+        else{document.getElementById('select_bg').style.width=243+'px'}
         let body_section=document.querySelector('.body_section')
         let main_body_section=document.querySelector('.main_body_section')
         let people_section=document.querySelector('.people_section')
