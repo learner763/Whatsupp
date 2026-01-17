b5164274fa8bbd62cae49aac1824546e8bd24fe7
umer <umeraftab763@gmail.com>
2026-01-17 16:54:58 +0500

    applied border on dp only when its empty and bg is not dark

--- src/Home.js
+++ src/Home.js
@@ -477,7 +477,7 @@ function Home()
             {
                 return {id:change.doc.id,...change.doc.data()}
             })
-            let deleted_messages=snapshot.docChanges().filter(change=> change.doc.data().delete || !on_reload.current?!change.doc.data().neondb:1).map(function(change)
+            let deleted_messages=snapshot.docChanges().filter(change=> change.doc.data().delete || (!on_reload.current?!change.doc.data().neondb:1)).map(function(change)
             {
                 return {id:change.doc.id,...change.doc.data()}
             })
