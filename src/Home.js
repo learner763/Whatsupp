f6b37638bb94286acd4076594935703693f03916
Umer Aftab <umeraftab763@gmail.com>
2026-06-24 15:56:19 +0500

    ui changes in home page

--- src/Home.js
+++ src/Home.js
@@ -937,16 +937,15 @@ function Home()
         }
     },[innerwidth,menu])
 
-    /*useEffect(()=>
+    useEffect(()=>
     {
         let body_section=document.querySelector('.body_section')
         let main_body_section=document.querySelector('.main_body_section')
         let people_section=document.querySelector('.people_section')
-        if(innerwidth<=1100 && innerwidth>500){body_section.style.height=(window.innerHeight-30)+'px';main_body_section.style.height=(window.innerHeight-30)+'px';people_section.style.height=(window.innerHeight-30)+'px'}
-        else if(innerwidth<=500){body_section.style.height=(window.innerHeight-40)+'px';main_body_section.style.height=(window.innerHeight-40)+'px';people_section.style.height=(window.innerHeight-40)+'px'}
+        if(innerwidth<=1100 && innerwidth>500){body_section.style.height=(window.innerHeight-78)+'px';main_body_section.style.height=(window.innerHeight-78)+'px';people_section.style.height=(window.innerHeight-78)+'px'}
+        else if(innerwidth<=500){body_section.style.height=(window.innerHeight-78)+'px';main_body_section.style.height=(window.innerHeight-78)+'px';people_section.style.height=(window.innerHeight-78)+'px'}
         else if(innerwidth>1100){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';people_section.style.height=(window.innerHeight)+'px'}
     },[innerheight,innerwidth])
-    */
 
     useEffect(() => {
         let container=document.getElementsByClassName('chat_detail_section');
