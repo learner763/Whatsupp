3087791f5a841c272c2f94dd341f2229c5879813
Umer Aftab <umeraftab763@gmail.com>
2026-06-27 10:23:17 +0500

    ui changes in home page

--- src/Home.js
+++ src/Home.js
@@ -942,9 +942,9 @@ function Home()
         let body_section=document.querySelector('.body_section')
         let main_body_section=document.querySelector('.main_body_section')
         let people_section=document.querySelector('.people_section')
-        if(innerwidth<=1100 && innerwidth>500){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';people_section.style.maxHeight=(window.innerHeight-78)+'px';people_section.style.minHeight=(window.innerHeight-78)+'px'}
-        else if(innerwidth<=500){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';people_section.style.maxHeight=(window.innerHeight-78)+'px';people_section.style.minHeight=(window.innerHeight-78)+'px'}
-        else if(innerwidth>1100){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';people_section.style.maxHeight=(window.innerHeight)+'px';people_section.style.minHeight=(window.innerHeight)+'px'}
+        if(innerwidth<=1100 && innerwidth>500){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';}
+        else if(innerwidth<=500){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';}
+        else if(innerwidth>1100){body_section.style.height=(window.innerHeight)+'px';main_body_section.style.height=(window.innerHeight)+'px';}
     },[innerheight,innerwidth])
 
     useEffect(() => {
